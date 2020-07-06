f4cca3bad4b86ebd95184c4cabbd3982dc4f033c
tle253 <tle253@dxc.com>
2020-07-06 13:55:21 +0700

    fix logout method

--- src/main/groovy/school/iu/clientserver/ClientServerApplication.groovy
+++ src/main/groovy/school/iu/clientserver/ClientServerApplication.groovy
@@ -2,11 +2,9 @@ package school.iu.clientserver
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
 @SpringBootApplication
-@EnableOAuth2Sso
 @EnableZuulProxy
 class ClientServerApplication {
 
--- src/main/groovy/school/iu/clientserver/UiSecurityConfig.groovy
+++ src/main/groovy/school/iu/clientserver/UiSecurityConfig.groovy
@@ -2,20 +2,23 @@ package school.iu.clientserver
 
 import java.security.Principal
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @Configuration
-@Order(101)
+@EnableOAuth2Sso
 class UiSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	void configure(HttpSecurity http) throws Exception {
-		http.cors().and().csrf().disable().antMatcher("/**")
-		.authorizeRequests().anyRequest().authenticated()
+		http.csrf().disable()
+        	.antMatcher("/**")
+            .authorizeRequests()
+            .anyRequest()
+            .authenticated()
 	}
 }
 
